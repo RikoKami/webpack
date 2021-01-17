ca20e99f6ecb5c738b105f4e625538a31b149491
RikoKami <orglary@gmail.com>
2021-01-17 20:45:56 -0300

    feat: add build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, "src", "index"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    }
-}
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+};
